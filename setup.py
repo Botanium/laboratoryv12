d605dae4709b83862107738a027a53d7a2acf71c
Botan Abdullah <botan.b.abdullah@gmail.com>
2023-04-26 12:10:36 +0300

    fixing the setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from setuptools.command.install import install
 import re, ast
 
 # get version from __version__ variable in laboratory/__init__.py
@@ -10,17 +10,17 @@ with open('laboratory/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
 
 setup(
-	name='laboratory',
-	version=version,
-	description='Laboratory App by Fwrat.com',
-	author='www.fwrat.com',
-	author_email='admin@fwrat.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+    name='laboratory',
+    version=version,
+    description='Laboratory App by Botan Abdullah',
+    author='Botan Burhan Abdullah',
+    author_email='botan.b.abdullah@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
 )
